fa5e803e9c42a724889fc5486f3b8dd1acbc40bd
k2nq2 <166155579+k2nq2@users.noreply.github.com>
2024-05-03 22:53:28 +0300

    Delete js/player.js

--- js/player.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('play', function(e) {
-  var audios = document.getElementsByTagName('audio');
-  for(var i = 0; i < audios.length; i++) {
-    if(audios[i] != e.target) {
-      audios[i].pause();
-    }
-  }
-}, true);
